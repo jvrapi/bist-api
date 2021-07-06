e3451cbcfebe233963a72c1d2aad590f077a7f97
Joao Vitor Santos <joaooviitorr@hotmail.com>
2021-07-06 13:53:55 -0300

    Updated ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -4,6 +4,9 @@ console.log('Database url: ', process.env.DATABASE_URL)
 module.exports = {
   type: process.env.BD_TYPE,
   url: process.env.DATABASE_URL,
+  extra: {
+    ssl: true
+  },
   entities: [rootDir + '/entities/*.{js,ts}'],
   migrations: [rootDir + '/database/migrations/*.{js,ts}'],
   cli: {
