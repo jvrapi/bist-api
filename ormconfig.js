4fdef83c42a4bbac8c1cfef8e831ba2f852493eb
Joao Vitor Santos <joaooviitorr@hotmail.com>
2021-07-06 14:09:24 -0300

    Updated ormconfig;

--- ormconfig.js
+++ ormconfig.js
@@ -2,9 +2,10 @@ const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
 module.exports = {
   type: process.env.BD_TYPE,
   url: process.env.DATABASE_URL,
-  extra: {
-    ssl: true
+  ssl: {
+    rejectUnauthorized: false
   },
+
   entities: [rootDir + '/entities/*.{js,ts}'],
   migrations: [rootDir + '/database/migrations/*.{js,ts}'],
   cli: {
