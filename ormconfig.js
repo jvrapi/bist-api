3b8143513bab0d3d2436e5ca257e93925ff24f36
Joao Vitor Santos <joaooviitorr@hotmail.com>
2021-07-06 13:33:49 -0300

    Ormconfig file updated

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,9 @@
 
 const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
+console.log('Database url: ', process.env.DATABASE_URL)
 module.exports = {
   type: process.env.BD_TYPE,
-  host: process.env.BD_HOST,
-  database: process.env.BD_DATABASE,
-  username: process.env.BD_USERNAME,
-  password: process.env.BD_PASSWORD,
-  port: process.env.BD_PORT,
-
+  url: process.env.DATABASE_URL,
   entities: [rootDir + '/entities/*.{js,ts}'],
   migrations: [rootDir + '/database/migrations/*.{js,ts}'],
   cli: {
