46031ec93f58e9aeeff8575a578c8772b0e3205b
Joao Vitor Santos <joaooviitorr@hotmail.com>
2021-08-18 15:10:39 -0300

    Added ssl option in typeorm config;

--- ormconfig.js
+++ ormconfig.js
@@ -2,9 +2,9 @@ const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
 module.exports = {
   type: process.env.BD_TYPE,
   url: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // },
+  ssl: {
+    rejectUnauthorized: false
+  },
 
   entities: [rootDir + '/entities/*.{js,ts}'],
   migrations: [rootDir + '/database/migrations/*.{js,ts}'],
