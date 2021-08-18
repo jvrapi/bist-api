373f1e1a9fb4949fe7eaacabaae3eb0abb77c00e
Joao Vitor Santos <joaooviitorr@hotmail.com>
2021-08-18 15:44:57 -0300

    Changed list product by name service; Created function to remove accents from strings;

--- src/services/ListProductsByNameService.ts
+++ src/services/ListProductsByNameService.ts
@@ -1,21 +1,20 @@
 import { getCustomRepository } from 'typeorm'
 import { ProductsRepository } from '../repositories/ProductsRepository'
+import { removeAccents } from '../utils/removeAccents'
 class ListProductByNameService {
   async execute(productName: string) {
     const repository = getCustomRepository(ProductsRepository)
+
     if (!productName) {
       throw new Error('Missing informations')
     }
-    const products = await repository.find({
-      take: 3
-    })
+
+    const products = await repository.find()
 
     const productsFiltered = products.filter(product =>
-      product.name
-        .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')
+      removeAccents(product.name)
         .toLowerCase()
-        .includes(productName.toLowerCase())
+        .includes(removeAccents(productName).toLowerCase())
     )
 
     return productsFiltered
